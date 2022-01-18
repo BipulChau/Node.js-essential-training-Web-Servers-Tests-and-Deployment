41ab26311aea4f1afbd04b7958bcfd6748950e13
Bipul Chaudhary <bipulchau@gmail.com>
2022-01-18 11:45:39 +0545

    Responds in plain tect

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 const http = require("http");
-http.createServer().listen(3000);
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World ");
+  })
+  .listen(3000);
+
+console.log("Web server is listening on Port 3000");
