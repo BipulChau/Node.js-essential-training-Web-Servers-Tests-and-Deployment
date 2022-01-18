68eef187d7cf09ccc911c72727a6bd727f93728a
Bipul Chaudhary <bipulchau@gmail.com>
2022-01-18 11:56:30 +0545

    Responds in html format

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const http = require("http");
 http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World ");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Creating web server</title>
+    </head>
+    <body>
+      <h1>Hello World</h1>
+    </body>
+    </html>
+    `);
   })
   .listen(3000);
 
