9d61ba356b1ad8f8d082d5e72b0c35e185721d6b
Bipul Chaudhary <bipulchau@gmail.com>
2022-01-18 13:26:09 +0545

    Collect Post data

--- Create_a_file_server/server_1.js
+++ Create_a_file_server/server_1.js
@@ -1,5 +1,6 @@
 const { createServer } = require("http");
 const { createReadStream } = require("fs");
+const { decode } = require("querystring");
 
 const sendFile = (res, status, type, filePath) => {
   res.writeHead(status, { "Content-Type": type });
@@ -7,9 +8,21 @@ const sendFile = (res, status, type, filePath) => {
 };
 
 createServer((req, res) => {
+  if (req.method === "POST") {
+    let body = "";
+    req.on("data", (data) => {
+      body += data;
+    });
+    req.on("end", () => {
+      const { name, email, message } = decode(body);
+      console.log(`${name} (${email}): ${message}`);
+    });
+  }
   switch (req.url) {
     case "/":
       return sendFile(res, 200, "text/html", "./home-page.html");
+    case "/contact":
+      return sendFile(res, 200, "text/html", "./contact.html");
     case "/img/dhyannalinga.jpeg":
       return sendFile(res, 200, "image/jpeg", "./Dhyanalinga.jpeg");
     case "/styles.css":
